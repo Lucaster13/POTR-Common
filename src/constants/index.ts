232e55530e8ebba44f5f8773a26e1171ed26a9c5
Lucas Terr <terr@usc.edu>
2024-03-24 00:32:08 -0400

    update potr-common npm registry

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -21,8 +21,7 @@ export const enum Status {
 // IPFS
 export const RAW_IPFS_URL_PREFIX = "ipfs://";
 export const RAW_IPFS_TEMPLATE_URL_PREFIX = "template-ipfs://";
-export const getIPFSTemplateURL = (potrId: string) =>
-	`${RAW_IPFS_TEMPLATE_URL_PREFIX}{ipfscid:1:dag-pb:reserve:sha2-256}/${potrId}.json`;
+export const IPFS_TEMPLATE_URL = `${RAW_IPFS_TEMPLATE_URL_PREFIX}{ipfscid:1:raw:reserve:sha2-256}`;
 export const IPFS_GATEWAY_URL_PREFIX = "https://ipfs.io/ipfs/";
 
 // POTR
